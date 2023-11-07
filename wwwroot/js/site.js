d09328b5855370f7e45d6eb2b65bd56db1737dbc
unknown <matheuspereira509pcr@gmail.com>
2023-11-07 19:52:36 -0300

    Primeiro Envio

---
+++ wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(
+     function(){
+        $("#formCadastro").submit(function (e)
+            {
+                e.preventDefault();
+                Cadastrar();
+            }
+        );
+      }
+     );
+     function Cadastrar()
+     {  
+     var parametros = {
+     Nome: $("#Idnome").val(),
+     Email: $("#Idemail").val(),
+     Mensagem: $("#Idmensagem").val(),
+     };
+
+     $.post("/Home/Cadastrar", parametros)
+
+     .done(function(data)
+     {
+      if(data.status == "Tudo Certo")
+      {
+     $("#formCadastro").after("<h3>Cadastro enviado com sucesso!</h3>");
+     $("#formCadastro").hide();
+      }
+      else{
+     alert(data.mensagem);
+       }
+      }
+     )
+     };
